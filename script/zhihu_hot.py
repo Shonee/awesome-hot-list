a7c671be61b05ccc245fc2c060a9ea35c00b5ac8
Adu <1048394082@qq.com>
2024-11-26 13:33:34 +0800

    Update zhihu_hot.py

--- script/zhihu_hot.py
+++ script/zhihu_hot.py
@@ -70,6 +70,6 @@ if __name__ == '__main__':
     result = get_zhihu_hot_json_by_api()
     print(result)
     date = get_current_date('%Y-%m')
-    save_result = json_to_csv_pandas(result, './data/csv/zhihu_hot_' + date + '.csv')
+    save_result = json_to_csv_pandas(result, 'data/csv/zhihu_hot_' + date + '.csv')
     print(save_result)
 
