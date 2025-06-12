194e1acdf712189e2450a8352ac9596d3e9b3c0e
Adu <1048394082@qq.com>
2025-06-12 10:46:08 +0800

    Update zhihu.py

--- script/zhihu.py
+++ script/zhihu.py
@@ -17,7 +17,8 @@ ZHIHU_PAGE_HOT_SEARCH = 'https://www.zhihu.com/topsearch'
 ZHIHU_API_HOT_LIST = "https://www.zhihu.com/api/v3/feed/topstory/hot-lists/total"
 
 HEADERs = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'cookie': os.environ.get("zhihu_cookie")
     }
 
 def get(url):
